eaba7e516713300ef17cddacfb0ac4137c77c203
asharaya singh bhadauriya <118126127+Aashray24092000@users.noreply.github.com>
2023-07-08 00:02:06 +0530

    Add files via upload

---
+++ decorators.py
@@ -0,0 +1,14 @@
+import time 
+
+def timer_test(func):
+    def timer():
+        start=time.time()
+        func()
+        end=time.time()
+        print(end-start)
+        print("welcome baby")
+    return timer
+@timer_test
+def test1():
+    print(564+32)
+test1()
