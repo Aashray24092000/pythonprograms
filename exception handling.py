1b4754a05efa884dec9b5774768b447bf323c6c6
asharaya singh bhadauriya <118126127+Aashray24092000@users.noreply.github.com>
2023-08-12 11:24:27 +0530

    Update exception handling.py

--- exception handling.py
+++ exception handling.py
@@ -14,3 +14,48 @@ try:
     validateage(age)
 except validateage as e:
     print(e)
+
+#ZeroDivisionError
+
+try:
+    a=10/0
+except ZeroDivisionError as e:
+    print(e)
+
+#valueerror
+try:
+    int("sudh")
+except (ValueError,TypeError) as e:
+    print(e)
+
+#importerror
+
+try:
+    import sudh
+except ImportError as e:
+    print(e)
+
+#keyerror
+
+try:
+    d={"key":"value",1:[1,2,3,4]}
+    print(d["key2"])
+except KeyError as e:
+    print(e)
+
+#attributeerror
+
+try:
+    "sudh".test()
+except AttributeError as e:
+    print(e)
+
+#indexerror
+
+try:
+    l=[1,2,3,44,5]
+    l[19]
+    print(l[19])
+except IndexError as e:
+    print(e)
+
